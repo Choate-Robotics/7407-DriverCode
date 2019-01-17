9ae90357ad8d25dcc2f0575faaaa55644904ce41
Annabelle Strong <annabellestrong@gmail.com>
2019-01-17 17:08:39 -0500

    Fomatting update

--- 7407 Driver Code/src/main/cpp/Robot.cpp
+++ 7407 Driver Code/src/main/cpp/Robot.cpp
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 #include "Robot.h"
 #include <iostream>
 #include <frc/smartdashboard/SmartDashboard.h>
@@ -41,8 +34,6 @@ void Robot::TeleopPeriodic() {
   else {
     m_RobotDrive.ArcadeDrive(m_stick.GetY(), m_stick.GetX());
   }
-
-
 }
 
 void Robot::TestPeriodic() {}
