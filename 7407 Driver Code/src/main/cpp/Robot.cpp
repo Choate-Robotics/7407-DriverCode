9080842986a77b2d1174a4857b9a2a7268b21259
Annabelle Strong <annabellestrong@gmail.com>
2019-01-17 16:46:43 -0500

    Joystick detection

--- 7407 Driver Code/src/main/cpp/Robot.cpp
+++ 7407 Driver Code/src/main/cpp/Robot.cpp
@@ -6,73 +6,43 @@
 /*----------------------------------------------------------------------------*/
 
 #include "Robot.h"
-
 #include <iostream>
-
 #include <frc/smartdashboard/SmartDashboard.h>
-
 #include <frc/Joystick.h>
 #include <frc/drive/DifferentialDrive.h>
 #include <frc/PWMVictorSPX.h>
+#include <frc/DoubleSolenoid.h>
 
 frc::PWMVictorSPX m_left{0};
 frc::PWMVictorSPX m_right{1};
 frc::DifferentialDrive m_RobotDrive(m_left, m_right);
-
 frc::Joystick m_stick{0};
+frc::DoubleSolenoid m_doubleSolenoid{0,1};
+
+std::string stick_type;
 
 void Robot::RobotInit() {
-  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+  stick_type = m_stick.GetName;
 }
 
-/**
- * This function is called every robot packet, no matter the mode. Use
- * this for items like diagnostics that you want ran during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
 void Robot::RobotPeriodic() {}
 
-/**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
- */
-void Robot::AutonomousInit() {
-  m_autoSelected = m_chooser.GetSelected();
-  // m_autoSelected = SmartDashboard::GetString(
-  //     "Auto Selector", kAutoNameDefault);
-  std::cout << "Auto selected: " << m_autoSelected << std::endl;
+void Robot::AutonomousInit() {}
 
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
-  }
-}
-
-void Robot::AutonomousPeriodic() {
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
-  }
-}
+void Robot::AutonomousPeriodic() {}
 
 void Robot::TeleopInit() {}
 
 void Robot::TeleopPeriodic() {
-  m_RobotDrive.ArcadeDrive(m_stick.GetY(), m_stick.GetX());
+  if(stick_type == "XboxJoystick")
+  {
+    //m_RobotDrive.TankDrive(m_stick.GetY(), m_stick.GetX());
+  } 
+  else {
+    m_RobotDrive.ArcadeDrive(m_stick.GetY(), m_stick.GetX());
+  }
+
+
 }
 
 void Robot::TestPeriodic() {}
