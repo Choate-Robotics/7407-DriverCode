ca941b49f909caf8c1232e1b9c283bdc727c1842
Annabelle Strong <annabellestrong@gmail.com>
2019-01-18 16:54:01 -0500

    Pneumatics additions

--- 7407 Driver Code/src/main/cpp/Robot.cpp
+++ 7407 Driver Code/src/main/cpp/Robot.cpp
@@ -1,6 +1,7 @@
 #include "Robot.h"
 #include <iostream>
 #include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/Timer.h>
 #include <frc/Joystick.h>
 #include <frc/drive/DifferentialDrive.h>
 #include <frc/PWMVictorSPX.h>
@@ -9,13 +10,32 @@
 frc::PWMVictorSPX m_left{0};
 frc::PWMVictorSPX m_right{1};
 frc::DifferentialDrive m_RobotDrive(m_left, m_right);
+
+frc::DoubleSolenoid m_solenoid_top{0, 1};
+frc::DoubleSolenoid m_solenoid_left{2, 3};
+frc::DoubleSolenoid m_solenoid_right{4, 5};
+
 frc::Joystick m_stick{0};
-frc::DoubleSolenoid m_doubleSolenoid{0,1};
 
 bool Logitech_Stick;
 
-void Robot::RobotInit() {
-  Logitech_Stick = true; //m_stick.GetIsXbox();
+void placeHatch()
+{
+  m_solenoid_top.Set(frc::DoubleSolenoid::kForward);
+  m_solenoid_left.Set(frc::DoubleSolenoid::kForward);
+  m_solenoid_right.Set(frc::DoubleSolenoid::kForward);
+}
+
+void releaseHatch()
+{
+  m_solenoid_top.Set(frc::DoubleSolenoid::kForward);
+  m_solenoid_left.Set(frc::DoubleSolenoid::kForward);
+  m_solenoid_right.Set(frc::DoubleSolenoid::kForward);
+}
+
+void Robot::RobotInit()
+{
+  Logitech_Stick = true; //m_stick->GetIsXbox();
 }
 
 void Robot::RobotPeriodic() {}
@@ -26,13 +46,30 @@ void Robot::AutonomousPeriodic() {}
 
 void Robot::TeleopInit() {}
 
-void Robot::TeleopPeriodic() {
-  if(Logitech_Stick == false)
+void Robot::TeleopPeriodic()
+{
+  if (Logitech_Stick == false)
   {
-    m_RobotDrive.TankDrive(m_stick.GetRawAxis(1), m_stick.GetRawAxis(5)); 
-  } 
-  else {
+    m_RobotDrive.TankDrive(m_stick.GetRawAxis(1), m_stick.GetRawAxis(5));
+  }
+  else
+  {
     m_RobotDrive.ArcadeDrive(m_stick.GetY(), m_stick.GetX());
+
+    if (m_stick.GetRawButton(5))
+    {
+      placeHatch();
+    }
+    else if (m_stick.GetRawButton(3))
+    {
+      releaseHatch();
+    }
+    else
+    {
+      m_solenoid_top.Set(frc::DoubleSolenoid::kOff);
+      m_solenoid_left.Set(frc::DoubleSolenoid::kOff);
+      m_solenoid_right.Set(frc::DoubleSolenoid::kOff);
+    }
   }
 }
 
@@ -41,3 +78,5 @@ void Robot::TestPeriodic() {}
 #ifndef RUNNING_FRC_TESTS
 int main() { return frc::StartRobot<Robot>(); }
 #endif
+
+
