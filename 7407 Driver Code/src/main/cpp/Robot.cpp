08f41915172902b51b173369a84ddd35011058eb
Annabelle Strong <annabellestrong@gmail.com>
2019-01-22 14:50:32 -0500

    Formatting

--- 7407 Driver Code/src/main/cpp/Robot.cpp
+++ 7407 Driver Code/src/main/cpp/Robot.cpp
@@ -5,9 +5,17 @@
 #include <frc/Joystick.h>
 #include <frc/drive/DifferentialDrive.h>
 #include <frc/PWMVictorSPX.h>
+#include <frc/PWMTalonSRX.h>
 #include <frc/SpeedControllerGroup.h>
 #include <frc/DoubleSolenoid.h>
 #include <string.h>
+
+/*
+frc::PWMTalonSRX leftFront{0};
+frc::PWMTalonSRX leftBack{1};
+frc::PWMTalonSRX rightFront{2};
+frc::PWMTalonSRX rightBack{3};
+*/
 
 frc::PWMVictorSPX leftFront{0};
 frc::PWMVictorSPX leftBack{1};
@@ -26,18 +34,25 @@ frc::Joystick stick{0};
 
 std::string Logitech_Stick;
 
-void placeHatch()
-{
-  solenoid_top.Set(frc::DoubleSolenoid::kForward);
-  solenoid_left.Set(frc::DoubleSolenoid::kForward);
-  solenoid_right.Set(frc::DoubleSolenoid::kForward);
-}
-
-void releaseHatch()
-{
-  solenoid_top.Set(frc::DoubleSolenoid::kForward);
-  solenoid_left.Set(frc::DoubleSolenoid::kForward);
-  solenoid_right.Set(frc::DoubleSolenoid::kForward);
+void Hatch(int button_1, int button_2){
+  if (stick.GetRawButton(button_1))
+  {
+    solenoid_top.Set(frc::DoubleSolenoid::kForward);
+    solenoid_left.Set(frc::DoubleSolenoid::kForward);
+    solenoid_right.Set(frc::DoubleSolenoid::kForward);
+  }
+  else if (stick.GetRawButton(button_2))
+  {
+    solenoid_top.Set(frc::DoubleSolenoid::kForward);
+    solenoid_left.Set(frc::DoubleSolenoid::kForward);
+    solenoid_right.Set(frc::DoubleSolenoid::kForward);
+  }
+  else
+  {
+    solenoid_top.Set(frc::DoubleSolenoid::kOff);
+    solenoid_left.Set(frc::DoubleSolenoid::kOff);
+    solenoid_right.Set(frc::DoubleSolenoid::kOff);
+  }
 }
 
 void Robot::RobotInit()
@@ -55,28 +70,14 @@ void Robot::TeleopInit() {}
 
 void Robot::TeleopPeriodic()
 {
-  if (Logitech_Stick != "Logitech Extreme 3D")
+  if (Logitech_Stick == "Logitech Extreme 3D")
   {
-    RobotDrive.TankDrive(stick.GetRawAxis(1), stick.GetRawAxis(5));
+    RobotDrive.ArcadeDrive(stick.GetY(), stick.GetX());
+    Hatch(5,3);
   }
   else
   {
-    RobotDrive.ArcadeDrive(stick.GetY(), stick.GetX());
-
-    if(stick.GetRawButton(5))
-    {
-      placeHatch();
-    }
-    else if (stick.GetRawButton(3))
-    {
-      releaseHatch();
-    }
-    else
-    {
-      solenoid_top.Set(frc::DoubleSolenoid::kOff);
-      solenoid_left.Set(frc::DoubleSolenoid::kOff);
-      solenoid_right.Set(frc::DoubleSolenoid::kOff);
-    }
+    RobotDrive.TankDrive(stick.GetRawAxis(1), stick.GetRawAxis(5));
   }
 }
 
